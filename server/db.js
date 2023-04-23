507f0bac7ae068918db68787949aa43e03831352
Ravi Sharma <76652256+Ravisharma7818@users.noreply.github.com>
2023-04-23 23:55:35 +0530

    Adding Env File

--- server/db.js
+++ server/db.js
@@ -1,16 +1,18 @@
-const mongoose = require("mongoose");
-dbConnect()
-async function dbConnect() {
+const mongoose = require('mongoose');
+
+const connectDB = async () => {
 
     try {
-        await mongoose.connect('mongodb://localhost:27017/RaviJobs', { useNewUrlParser: true });
-        console.log('Mongo DB Connection success')
+        const conn = await mongoose.connect(process.env.MY_MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+        })
+        console.log(`mongo database is connected!!!`)
     } catch (error) {
-        console.log('Mongo DB Connection failed')
+        console.error(`Error: ${error} `)
+        process.exit(1) //passing 1 - will exit the proccess with error
     }
 
 }
 
-module.exports = mongoose
-
-
+module.exports = connectDB
--- server/index.js
+++ server/index.js
@@ -1,9 +1,11 @@
 const express = require("express");
 const app = express();
-const db = require("./db.js");
+const connectDB = require("./db.js");
 const path = require("path");
 const jobsRoute = require('./routes/jobsRoute')
 const userRoute = require('./routes/usersRoute')
+const dotenv = require('dotenv').config()
+
 app.use(express.json())
 
 
@@ -11,6 +13,6 @@ app.use('/api/jobs/', jobsRoute)
 app.use('/api/users/', userRoute)
 const port = process.env.PORT || 5000;
 
-
+connectDB();
 
 app.listen(port, () => console.log('Node JS Server Started'));
